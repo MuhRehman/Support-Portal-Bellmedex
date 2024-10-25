2fcb3c9620cafad740c0618849f12d397ae81fc8
suhaib afridi <lpt009@prosoft.com>
2024-10-25 10:56:53 -0700

    navbar styling

--- src/Components/StatusComp.jsx
+++ src/Components/StatusComp.jsx
@@ -9,7 +9,29 @@
       const [error, setError] = useState(null);
       const [loading, setLoading] = useState(true);
   
-      
+      const dataStatus1 = [
+        {
+         id: "e1",
+        title: "Toilet Paper",
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+        },
+        { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+        { 
+        id: "e3",
+        title: "Car Insurance",
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+        },
+        {
+        id: "e4",
+        title: "New Desk (Wooden)",
+        amount: 450,
+        date: new Date(2021, 5, 12),
+         },
+         ]; 
+
+
     useEffect(() => {
       const fetchData = async () => {
         try {
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -26,9 +26,6 @@ export default function Dashboard() {
         setData(response.data); 
 		console.log(response.data);
 
-		const response2 = await axios.get('https://tms-api.providerscredentialing.com/api/Dashboard/GetTasksByPriority');
-        setDataPriority(response2.data); 
-		console.log("DataPriority: ", response2.data);
 		
       } catch (error) {
         setError(error.message); 
@@ -52,91 +49,56 @@ export default function Dashboard() {
   	
 <header>
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Logo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-
-    
-
-{/* <div class="navigation-wrap bg-light start-header start-style">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<nav class="navbar navbar-expand-md navbar-light">
-					
-					
-						
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-							<span class="navbar-toggler-icon"></span>
-						</button>
-						
-						<div class="collapse navbar-collapse" id="navbarSupportedContent">
-							<ul class="navbar-nav ml-auto py-4 py-md-0">
-								
-								<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-									<a class="nav-link" href="#">Portfolio</a>
-								</li>
-								<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-									<a class="nav-link" href="#">Agency</a>
-								</li>
-								
-								<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-									<a class="nav-link" href="#">Journal</a>
-								</li>
-								<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-									<a class="nav-link" href="#">Contact</a>
-								</li>
-							</ul>
-						</div>
-						
-					</nav>		
-				</div>
-			</div>
-		</div>
-	</div> */}
-	<nav class="navbar navbar-expand-lg navbar-light bg-white custom-nav">
-		{/* <i class="fa fa-bars mobile-menu"></i>	 */}
- 		 <a class="navbar-brand" href="#">Logo</a>
-		  	<form class="form-inline search-form">
-		  		{/* <i class="fa fa-search"></i>
-		      <input class="form-control " type="search" placeholder="Search..."/> */}
-			  <ul>
-			  <div class="d-flex">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link ml-3" href="#">Dashboard <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link ml-3" href="#"> Tickets</a>
+      </li>
+      {/* <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li> */}
+      <li class="nav-item">
+        <a class="nav-link ml-3">Settings		</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+	<div class="d-flex">
 			        <a class="nav-link user-prfile" href="#">
 			        	<img src="https://avatar.iran.liara.run/public" alt=""/>
 			        </a>
 					<div className='mt-3 font-weight-bold'>Avater</div>
-							    </div>
-				
-			  </ul>
-		    </form>
-		    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-			     <li class="drop">
-				      <a class="nav-link bell-icon-badge" href="#">
+
+					<a class="nav-link bell-icon-badge" href="#">
 			        	<i class="fa fa-bell-o" aria-hidden="true"></i>
 			        	<span class="bell-badge">10</span>
 			        </a>
+							    </div>
+     
+   
+    </form>
+  </div>
+</nav>
+	
 
-				      <ul class="drop-menu">
-				        <li><a href="#">Art</a></li>
-				        <li><a href="">Coding</a></li>
-				        <li><a href="">Design</a></li>
-				        <li><a href="">Web Development</a></li>
-				    </ul>
-				</li>
 
-			    <li class="nav-item">
-			        {/* <a class="nav-link user-prfile" href="#">
-			        	<img src="https://avatar.iran.liara.run/public" alt=""/>
-			        </a> */}
-			    </li>
 
-				{/* <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-									<a class="nav-link" href="#">Journal</a>
-								</li>
-								<li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-									<a class="nav-link" href="#">Contact</a>
-								</li> */}
-		</ul>
-	</nav>
 </header>
 
 <div class="container-fluid">
@@ -286,6 +248,8 @@ export default function Dashboard() {
 		</main>
 	</div>
 </div>
+
+
     </>
   )
 }
